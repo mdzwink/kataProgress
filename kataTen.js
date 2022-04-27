8871c700334236df3da37826ea7b8e69e5f5add7
mdzwink <mdzwink@gmail.com>
2022-04-26 23:23:12 -0400

    tiiin

---
+++ kataTen.js
@@ -0,0 +1,13 @@
+const multiplicationTable = function(maxValue) {
+  // Your code here
+};
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
+
+
+
+//Create a function named 'multiplicationTable' that receives a number 
+//..'maxValue' as input and creates a square multiplication table where 
+//..'maxValue' is the largest value in the table.
